c7d3bc39adf899aca62428e44269f62ab6d32414
= <devhernandojunior@gmail.com>
2024-06-18 15:22:27 -0300

    repetição past

---
+++ Repetição/for.js
@@ -0,0 +1,4 @@
+//FOR
+for (let number2 = 0; number2 <= 9; number2++) {
+  console.log(`${number2}`)
+}
---
+++ Repetição/while.js
@@ -0,0 +1,11 @@
+//WHILE
+let number = 0
+while (number >= 0 && number <= 9) {
+  console.log(`${number}`)
+  number += 1
+}
+
+//FOR
+for (let number2 = 0; number2 <= 9; number2++) {
+  console.log(`${number2}`)
+}
